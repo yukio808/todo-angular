355dbfa740819afaa42431998c458067ddd62e32
yukio yamamoto <aj.yamamoto@me.com>
2015-03-09 12:01:53 -1000

    adds express server

---
+++ server/controllers/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.send('hello API');
+});
+
+
+//add todo
+
+
+//delete todo
+
+
+//complete todo
+
+
+//uncomplete todo
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/angulartodo');
+
+app.use(express.static(__dirname + './../public'));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use('/api', require('./controllers/api'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
